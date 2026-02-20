4b621ce3adbcf03d92b57415a6a5507ddeaefa38
Anushka Bukkawar <anushkabukkawar64@gmail.com>
2026-02-20 13:39:10 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,38 @@
+import {createAgent } from 'langchain'
+import { ChatOllama } from '@langchain/ollama';
+
+import { TavilySearch } from "@langchain/tavily";
+
+const accessInternet = new TavilySearch({
+
+  maxResult: 5,
+  topic: "news",
+  searchdepth: "Advance",
+  tavilyApiKey: "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",
+
+})
+
+let ollama = new ChatOllama({
+    model: "llama3.1",
+    temperature: 0.1
+})
+
+let agent = createAgent({
+    model: ollama,
+    tools: [accessInternet]
+})
+
+const run = async () => {
+    try {
+        let response = await agent.invoke({
+            messages: "meaning of name sneha"
+        })
+
+    console.log(response.messages[1].content);
+        console.log(response);
+    } catch (e) {
+        console.log(e);
+    }
+}
+
+run();
